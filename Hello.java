3a9bf5449d80642b08a8e86d78e02736b2355b46
G.J.Zhou <1191984013@qq.com>
2021-04-22 16:42:12 +0800

    my first java program

--- Hello.java
+++ Hello.java
@@ -1,5 +1,16 @@
+
+//这是java的快速入门，演示Java的开发步骤
+//对代码的相关说明
+//1.public class Hello 表示Hello是一个public公有的类
+//2.Hello{}表示一个类的开始和结果
+//3.public static void main表示主方法，即程序的入口
+//4.main(){}方法的开始和结束
+//5.System.out.println("hello,world~")输出“Hello world”到屏幕
+//6.";"表示语句结束
 public class Hello {
-public static void main(String[] args) {
-	System.out.println("Hello world!");
-}
+	
+	//编写一个main方法
+	public static void main(String[] args) {
+		System.out.println("G.J.Zhou is studying java!");
+	}
 }
